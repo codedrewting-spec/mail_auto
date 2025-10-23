7ff66c7b69611c59103527db28c48ad5a6e28baa
codedrewting-spec <codedrewting@gmail.com>
2025-10-24 04:06:42 +0800

    Provide workspace dev script

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Placeholder: React.FC = () => {
+  return <div>Focusflow UI placeholder component</div>;
+};
+
+export default Placeholder;
---
+++ packages/web/scripts/dev-placeholder.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+console.log("@focusflow/web dev server placeholder");
+console.log("No web application is wired up yet. Add your framework and update this script to start it.");
